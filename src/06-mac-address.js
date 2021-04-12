79cc8ed1240af00dc04a4da1655455d3a6a5d00f
Yerzhan Khabulov <SESA533386@se.com>
2021-04-12 20:33:14 +0500

    feat: add mac adress task

--- src/06-mac-address.js
+++ src/06-mac-address.js
@@ -9,11 +9,14 @@
  * @return {Number}
  *
  * @example
- * For 00-1B-63-84-45-E6, the output should be true.
+ * For  , the output should be true.
  *
  */
-function isMAC48Address(/* n */) {
-  throw new Error('Not implemented');
+function isMAC48Address(n) {
+  const reg = /^[0-9A-F]{2}-[0-9A-F]{2}-[0-9A-F]{2}-[0-9A-F]{2}-[0-9A-F]{2}-[0-9A-F]{2}$/;
+  if (n.match(reg)) {
+    return true;
+  } return false;
 }
 
 module.exports = isMAC48Address;
